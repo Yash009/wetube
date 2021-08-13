42c2c8b7503c10609432ed8ecdc312615fb290c2
Unknown <yash.900209@gmail.com>
2021-08-13 14:02:44 -0400

    Added video.js

---
+++ src/components/Video.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Video = () => {
+  return  (
+    <div>
+    <h2>Title of the video</h2>
+    <h3>Date Added</h3>
+    <h3>Channel name</h3>
+    </div>
+  )
+}
+export default Video;
